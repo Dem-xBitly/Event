709eeede532120ce3b47a3782540b7d830ca5f39
xBitly <xBitly@mail.ru>
2021-04-09 23:36:37 +0300

    update drawable and layout

--- app/src/main/java/dem/xbitly/eventplatform/SettingsActivity.java
+++ app/src/main/java/dem/xbitly/eventplatform/SettingsActivity.java
@@ -1,12 +1,9 @@
 package dem.xbitly.eventplatform;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import dem.xbitly.eventplatform.databinding.ActivitySettingsBinding;
 
@@ -17,22 +14,16 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.backFromSettingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
-            }
-        });
+        binding.backFromSettingsBtn.setOnClickListener(v ->
+                startActivity(new Intent(SettingsActivity.this, MainActivity.class)));
 
-        binding.toProfileSettingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SettingsActivity.this, ProfileSettings.class));
-            }
-        });
+        binding.toProfileSettingsBtn.setOnClickListener(v ->
+                startActivity(new Intent(SettingsActivity.this, ProfileSettings.class)));
+
 
     }
 }
--- app/src/main/java/dem/xbitly/eventplatform/StartActivity.java
+++ app/src/main/java/dem/xbitly/eventplatform/StartActivity.java
@@ -1,11 +1,9 @@
 package dem.xbitly.eventplatform;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import dem.xbitly.eventplatform.databinding.ActivityStartBinding;
 
@@ -19,20 +17,14 @@ public class StartActivity extends AppCompatActivity {
         binding = ActivityStartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.toSignInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in_intent = new Intent (StartActivity.this, LoginActivity.class);
-                startActivity(in_intent);
-            }
+        binding.toSignInBtn.setOnClickListener(v -> {
+            Intent in_intent = new Intent (StartActivity.this, LoginActivity.class);
+            startActivity(in_intent);
         });
 
-        binding.toSignUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent up_intent = new Intent (StartActivity.this, RegisterActivity.class);
-                startActivity(up_intent);
-            }
+        binding.toSignUpBtn.setOnClickListener(v -> {
+            Intent up_intent = new Intent (StartActivity.this, RegisterActivity.class);
+            startActivity(up_intent);
         });
     }
 }
